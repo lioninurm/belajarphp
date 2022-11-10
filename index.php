fc5d596aa21caa1a066ff5405350cd51fda3768a
lioninurm <lioninurmahfudah@gmail.com>
2022-11-10 11:31:18 +0700

    php1

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
 
+    echo "Hello, world!<br>";
 
+    $nama = "Lioni";
+    $umur = "20";
+    
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
 
+    $NamaKakak = "Anisa";
+    $UmurKakak = "26";
 
+    echo "Nama kakak saya <strong>$NamaKakak</strong>, dia berusia $UmurKakak tahun.<br>";
+
+    $SelisihUmur = $UmurKakak - $umur;
+
+    echo "Selisih umur kami adalah $SelisihUmur tahun.";
 
 ?>
 
