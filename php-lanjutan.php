9b252ada51e813e1a7e9bd889e2ad3cbb34a8373
lioninurm <lioninurmahfudah@gmail.com>
2022-11-10 15:32:28 +0700

    php2

---
+++ php-lanjutan.php
@@ -0,0 +1,27 @@
+<?php
+$nama = "Lioni Nur Mahfudah";
+
+/*
+echo $nama;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+echo $nama;
+*/
+
+
+//perulangan//
+
+
+$no = 15;
+for ($i=0; $i<$no; $i++) {
+    $n = $i + 1;
+    echo $n." ".$nama."<br/>";
+}
+
+
+?>
